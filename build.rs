4cf73d1fb0365cf9217f84c9bb25eadb7ed0a5e7
Sven-Hendrik Haase <svenstaro@gmail.com>
2020-10-07 10:31:21 +0200

    Fix build on docs.rs (fixes #18)
    The build fails because build.rs tries to download an updated bank list file which fails
    on docs.rs since it doesn't allow network access during builds.

--- build.rs
+++ build.rs
@@ -6,12 +6,18 @@ static HBCI4J_COMMIT: &str = "dfc8f67773580d9ba2c20f8b9876355364f46ffe";
 fn main() -> Result<(), Box<dyn std::error::Error>> {
     let final_url = format!(
         "https://raw.githubusercontent.com/hbci4j/hbci4java/{commit}/src/main/resources/blz.properties",
-	commit=HBCI4J_COMMIT,
+        commit=HBCI4J_COMMIT,
     );
-    let mut res = reqwest::blocking::get(&final_url)?;
 
     let mut buf: Vec<u8> = vec![];
-    res.copy_to(&mut buf)?;
+
+    if env::var("DOCS_RS").is_err() {
+        let mut res = reqwest::blocking::get(&final_url)?;
+        res.copy_to(&mut buf)?;
+    } else {
+        // Fake data for docs.rs because it doesn't have network access.
+        buf.push(88);
+    }
 
     let filename = final_url.split('/').last().unwrap();
     let out_file = format!(
